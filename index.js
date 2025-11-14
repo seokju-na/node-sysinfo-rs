cb0718929207798783230e1186ae38637f68c451
Seokju Na <seokju.me@toss.im>
2025-11-14 19:17:51 +0900

    0.1.3

--- index.js
+++ index.js
@@ -558,7 +558,7 @@ if (!nativeBinding) {
   if (loadErrors.length > 0) {
     const messages = loadErrors.map((err) => err.message).join('\n')
     throw new Error(
-      `${process.platform} ${process.arch} Cannot find native binding: ${messages}`
+      `${process.platform} ${process.arch} Cannot find native binding: ${messages}`,
       {
         cause: loadErrors.reduce((err, cur) => {
           cur.cause = err
